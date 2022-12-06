163fd0bb17060921ed3026dfb1536b7ab9ddce1b
Guilherme <guilhermefreire841@gmail.com>
2022-12-06 17:16:47 -0300

    Commit - Upload do projeto_zap

---
+++ projeto_zap/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_zap/view/home_view.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Projeto_Zap',
+    debugShowCheckedModeBanner: false,
+    home: HomeView(),
+  ));
+}
